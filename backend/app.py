0cf261dd09fd4ddb10981c32318d5889a15d2062
KaryampudiMadhav <karyampudimadhav@gmail.com>
2025-07-22 20:39:10 +0530

    made server and frontend run on same server

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask,send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +11,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+frontend_path = os.path.join(os.getcwd(),"..","frontend")
+dist_path = os.path.join(frontend_path,"dist")
+@app.route("/",defaults={"filename" : ""})
+@app.route("/<path:filename>")
+def index(filename):
+ if not filename:
+  filename = 'index.html'
+ return send_from_directory(dist_path,filename)
 import routes
 
 with app.app_context():
